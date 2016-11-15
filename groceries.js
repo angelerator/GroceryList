a446c55d569697f047ccf39750707c00a67c3a86
darin <dcroft1@mail.csuchico.edu>
2016-11-14 22:55:37 -0800

    first half of x23 done

--- groceries.js
+++ groceries.js
@@ -2,7 +2,16 @@ function addItem(){
   var input = document.getElementById("newItem").value;
   var list = document.getElementById("listDisplay");
   var item = document.createElement("li");
+  var btnClose = document.createElement("button");
+    btnClose.classList.add("btn");
+    btnClose.classList.add("btn-danger");
+    btnClose.classList.add("btn-xs");
+  var iconClose = document.createElement("span");
+    iconClose.classList.add("glyphicon");
+    iconClose.classList.add("glyphicon-remove");
   var itemName = document.createTextNode(input);
+  btnClose.appendChild(iconClose);
+  item.appendChild(btnClose);
   item.appendChild(itemName);
   list.appendChild(item);
   document.getElementById("newItem").value = "";
